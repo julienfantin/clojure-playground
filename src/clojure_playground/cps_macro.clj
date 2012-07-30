b0f7b74e858603756fe248adcecb3d63ca2a1b0c
Julien Fantin <julienfantin@gmail.com>
2012-07-30 12:05:56 -0400

    Macro cleanup.

--- src/clojure_playground/cps_macro.clj
+++ src/clojure_playground/cps_macro.clj
@@ -4,13 +4,28 @@
   (let [[sym _] (first bindings)
         [_ expr] (second bindings)]
     `(fn [~sym]
-       (do
-         ~(if expr
-            `(~expr ~(doasync- (next bindings) forms))
-            forms)
-         nil))))
+       ~(if expr
+          `(~expr ~(doasync- (next bindings) forms))
+          `(do ~@forms))
+       nil)))
 
 (defmacro doasync
-  ([bindings & forms]
-     (let [bindings (cons [(gensym) nil] (partition 2 bindings))]
-       `(~(doasync- bindings forms) nil))))
+  "Continuation passing style let form.
+
+Bindings => binding-form cps-expr
+
+Destructuring is supported in `bindings`, but the cps-exprs must
+evaluate to a function of one argument, the current continuation. Each
+expression is responsible for applying the continuation to its result.
+
+doasync will always return nil, thus `forms` should only be used for
+side-effects.
+
+(doasync [a (fn [c] (c 1))
+          b (fn [c] (c (inc a)))]
+         (println b))
+; nil
+;; => 2"
+[bindings & forms]
+(let [bindings (cons [(gensym) nil] (partition 2 bindings))]
+  `(~(doasync- bindings forms) nil)))
